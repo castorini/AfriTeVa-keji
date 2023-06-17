88b0a8242c24b0f6d1e5879e81deae61fd80498b
theyorubayesian <akin.o.oladipo@gmail.com>
2023-06-17 17:17:32 +0000

    add finetuning and eval scripts

--- scripts/lafand_mt.sh
+++ scripts/lafand_mt.sh
@@ -1,26 +1,96 @@
-export CUDA_VISIBLE_DEVICES="3"
-export NCCL_P2P_DISABLE=1 # May be necessary on CUDA 12.
+set -x;
+set -n;
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
 
-bash scripts/t5_utils.sh \
-finetune \
-base \
-lafand_mt \
-runs/T5_1_1_base_lafand \
---gin.TASK_FEATURE_LENGTHS="{'inputs': 512, 'targets': 200}" \
---gin.TRAIN_STEPS=564288 \
---gin.BATCH_SIZE=1 \
---gin.EVAL_PERIOD=3000 \
---gin.train.infer_eval_dataset_cfg=None \
---gin.utils.SaveCheckpointConfig.period=3000 \
->& logs/T5_1_1_base_lafand_ft.log &
+# Pass `true` if you you set env var `DATA_GCP_DIR` to a local path on your machine
+USING_LOCAL_DATASET=true
+# Pass full bucket dir for dataset if dataset is not on local.
+DATASET_DIR=data/lafand
+TRAIN_BATCH_SIZE=32
+EVAL_BATCH_SIZE=64
+INFER_BATCH_SIZE=64
+CHECKPOINT="gs://awarawa/T5_1_1_base/checkpoint_524288"
+CHECKPOINT_PERIOD=auto
+MODEL_SIZE="base"
+EVAL_PERIOD=auto
+# Please pass FEATURE_LENGTHS as string dictionary.
+FEATURE_LENGTHS="{'inputs': 512, 'targets': 200}"
+# We pretrained for 524288 steps if you use the final checkpoints.
+# If you use any other checkpoint, take note of its pre-trained steps.
+PRETRAINED_STEPS=524288
+FT_NUM_EPOCHS=5
+OUTPUT_DIR="arawat5_base_lafand"
+mkdir -p logs/$OUTPUT_DIR
+REMOVE_CHECKPOINTS=true
+# ---------------------------------------------
 
+LANGUAGES=("hau")
 
-bash scripts/t5_utils.sh \
-eval \
-base \
-lafand_mt \
-gs://awarawa/T5_1_1_base_lafand/eval_564288 \
---gin.TASK_FEATURE_LENGTHS="{'inputs': 512, 'targets': 200}" \
---gin.CHECKPOINT_PATH=\"gs://awarawa/T5_1_1_base_lafand/checkpoint_564288\" \
---gin.utils.DatasetConfig.batch_size=16 \
->& logs/T5_1_1_base_lafand_eval.log &
+for language in ${LANGUAGES[@]}
+do
+    # TODO: You can check the task name format in src/teva/tasks.py
+    for task in "${language}_en_lafand_mt" # "en_${language}_lafand_mt"
+    do
+        if [[ $USING_LOCAL_DATASET == "true" ]]; then
+            num_examples=$(wc -l $DATASET_DIR/en-${language}/train.json | cut -f 1 -d " ")
+        else
+            num_examples=$(gsutil cat $DATASET_DIR/en-${language}/train.json | wc -l)
+        fi
+
+        num_steps_per_epoch=$((num_examples/TRAIN_BATCH_SIZE))
+        ft_steps=$((FT_NUM_EPOCHS * num_steps_per_epoch))
+        # TRAIN_STEPS MUST ALWAYS BE pre-trained steps + no. of fine-tuning steps.
+        train_steps=$((PRETRAINED_STEPS + num_steps_per_epoch))
+
+        [[ $EVAL_PERIOD == "auto" ]] && EVAL_PERIOD=$num_steps_per_epoch
+        [[ $CHECKPOINT_PERIOD == "auto" ]] && CHECKPOINT_PERIOD=$num_steps_per_epoch
+
+        for seed in 6
+        do
+            seed_output_dir=runs/$OUTPUT_DIR/${task}_${seed}
+
+            # TODO: Remove the `--cuda_12` command if you're not on CUDA 12
+            # --gin.infer_eval/utils.DatasetConfig.batch_size=$INFER_BATCH_SIZE \
+            bash scripts/t5_utils.sh \
+            --action finetune \
+            --task $task \
+            --feature_lengths "$FEATURE_LENGTHS" \
+            --batch_size $TRAIN_BATCH_SIZE \
+            --checkpoint $CHECKPOINT \
+            --checkpoint_period $CHECKPOINT_PERIOD \
+            --eval_period $EVAL_PERIOD \
+            --train_steps $train_steps \
+            --model_size $MODEL_SIZE \
+            --output_dir $seed_output_dir \
+            --cuda_12 \
+            --no_infer_eval \
+            > logs/$OUTPUT_DIR/${task}_${seed}_ft.log \
+            && finetuned=true
+
+            checkpoints=($(ls $seed_output_dir | grep checkpoint | grep -v "524288"))
+
+            # Uncomment if you are using `no_infer_eval` when finetuning.
+            for checkpoint in ${checkpoints[@]}
+            do
+                checkpoint_steps=${checkpoint##*_}
+                bash scripts/t5_utils.sh \
+                --action eval \
+                --task $task \
+                --feature_lengths "$FEATURE_LENGTHS" \
+                --model_size $MODEL_SIZE \
+                --checkpoint $seed_output_dir/$checkpoint \
+                --batch_size $EVAL_BATCH_SIZE \
+                --output_dir $seed_output_dir/eval_${checkpoint_steps} \
+                --cuda_12 \
+                >> logs/$OUTPUT_DIR/${task}_${seed}_eval.log
+            done
+
+            if [[ $REMOVE_CHECKPOINTS == "true" ]]; then
+                for ckpt in ${checkpoints[@]};
+                do
+                    rm -rf $ckpt
+                done
+            fi
+        done
+    done
+done
--- scripts/t5_utils.sh
+++ scripts/t5_utils.sh
@@ -1,11 +1,4 @@
-ACTION=$1 && shift
-MODEL_SIZE=$1 && shift
-TASK=$1 && shift
-OUTPUT_DIR=$1 && shift
-
-if [ $# -gt 0 ]; then
-    # We assume the remaining arguments passed are gin configurations
-    echo "
+echo "
         Note: when supplying a string, dict, list, or tuple value via a flag, you must put it in quotes. 
         In the case of strings, it requires escaped quotes (\\\"<string>\\\"). 
         For example: 
@@ -13,9 +6,84 @@ if [ $# -gt 0 ]; then
         --gin.utils.DatasetConfig.task_feature_lengths=\"{'inputs': 512, 'targets': 84}\", and 
         --gin.dense.MlpBlock.activations=\"('dense', 'gelu')\"
     "
+
+# -----------------
+# Default arguments
+# -----------------
+batch_size=16
+eval_period=5000
+checkpoint_period=5000
+model_size="base"
+
+# --- 
+# CLI
+# ---
+ensure() {
+    [[ -z "$2" ]] && echo "ERROR: \`$1\` is null or unset" && exit
+}
+
+other_configs=()
+
+while [ "$1" != "" ]; do
+    echo $1
+    # Collect any additional gin configs and pass to action command
+    [[ $1 == --gin* ]] && other_configs+=($1) && shift && continue
+
+    case $1 in
+        -a | --action )                 shift
+                                        action=$1
+                                        ;;
+        -b | --batch_size )             shift
+                                        batch_size=$1
+                                        ;;
+        -c | --checkpoint )             shift
+                                        checkpoint=\"$1\"
+                                        ;;
+        -cp | --checkpoint_period )     shift
+                                        checkpoint_period=$1
+                                        ;;
+        --cuda_12 )                     export NCCL_P2P_DISABLE=1
+                                        ;;
+        --no_infer_eval )               no_infer_eval=false
+                                        ;;
+        -e | --eval_period )            shift
+                                        eval_period=$1
+                                        ;;
+        -f | --feature_lengths )        shift
+                                        feature_lengths=$1
+                                        ;;
+        -m | --model_size )             shift
+                                        model_size=$1
+                                        ;;
+        --task )                         shift
+                                        task=$1
+                                        ;;
+        -t | --train_steps )            shift
+                                        train_steps=$1
+                                        ;;
+        -o | --output_dir )             shift
+                                        output_dir=$1
+                                        ;;
+        -h | --help )                   exit    # TODO: @theyorubayesian
+                                        ;;
+    esac
+    shift
+done
+
+ensure "action" $action
+ensure "output_dir" $output_dir
+ensure "task" $task
+ensure "checkpoint" $checkpoint
+ensure "feature_lengths" $feature_lengths
+
+if [[ $action == "pretrain" || $action == "finetune" ]]; then
+    ensure "train_steps" $train_steps
 fi
 
-case $MODEL_SIZE in
+echo ${other_configs[@]}
+# ----------------------------------- #
+
+case $model_size in
     base)
         ;;
     large)
@@ -26,48 +94,62 @@ case $MODEL_SIZE in
         ;;
 esac
 
-case $ACTION in
+case $action in
     pretrain)
         set -x;
         python3 -m t5x.main \
         --run_mode="train" \
-        --gin_file="config/models/t5_1_1/$MODEL_SIZE.gin" \
+        --gin_file="config/models/t5_1_1/$model_size.gin" \
         --gin_file="config/runs/t5_1_1/pretrain.gin" \
-        --gin.MIXTURE_OR_TASK_NAME=\"${TASK}\" \
-        --gin.MODEL_DIR=\"${OUTPUT_DIR}\" \
-        "$@" \
+        --gin.MIXTURE_OR_TASK_NAME=\"${task}\" \
+        --gin.TASK_FEATURE_LENGTHS="$feature_lengths" \
+        --gin.TRAIN_STEPS=${train_steps} \
+        --gin.BATCH_SIZE=${batch_size} \
+        --gin.train.eval_period=${eval_period} \
+        --gin.utils.SaveCheckpointConfig.period=${checkpoint_period} \
+        --gin.MODEL_DIR=\"${output_dir}\" \
+        "${other_configs[@]}" \
         --alsologtostderr
         ;;
     finetune)
         set -x;
         python3 -m t5x.train \
-        --gin_file="config/models/t5_1_1/$MODEL_SIZE.gin" \
+        --gin_file="config/models/t5_1_1/$model_size.gin" \
         --gin_file="config/runs/t5_1_1/finetune.gin" \
-        --gin.MIXTURE_OR_TASK_NAME=\"${TASK}\" \
-        --gin.MODEL_DIR=\"${OUTPUT_DIR}\" \
-        "$@" \
+        --gin.MIXTURE_OR_TASK_NAME=\"${task}\" \
+        --gin.TASK_FEATURE_LENGTHS="${feature_lengths}" \
+        --gin.TRAIN_STEPS=${train_steps} \
+        --gin.BATCH_SIZE=${batch_size} \
+        --gin.INITIAL_CHECKPOINT_PATH=${checkpoint} \
+        --gin.EVAL_PERIOD=${eval_period} \
+        --gin.utils.SaveCheckpointConfig.period=${checkpoint_period} \
+        --gin.MODEL_DIR=\"${output_dir}\" \
+        "${other_configs[@]}" \
+        ${no_infer_eval+--gin.train.infer_eval_dataset_cfg=None} \
         --alsologtostderr 
         ;;
     eval)
         set -x;
         python3 -m t5x.eval \
-        --gin_file="config/models/t5_1_1/$MODEL_SIZE.gin" \
+        --gin_file="config/models/t5_1_1/$model_size.gin" \
         --gin_file="config/runs/t5_1_1/eval.gin" \
-        --gin.MIXTURE_OR_TASK_NAME=\"${TASK}\" \
-        --gin.EVAL_OUTPUT_DIR=\"${EVAL_OUTPUT_DIR}\" \
+        --gin.MIXTURE_OR_TASK_NAME=\"${task}\" \
+        --gin.TASK_FEATURE_LENGTHS="${feature_lengths}" \
+        --gin.CHECKPOINT_PATH=${checkpoint} \
+        --gin.EVAL_OUTPUT_DIR=\"${output_dir}\" \
         --alsologtostderr \
-        "$@"
+        "${other_configs[@]}"
         ;;
     infer)
         set -x;
         python3 -m t5x.infer \
-        --gin_file="config/models/t5_1_1/$MODEL_SIZE.gin" \
+        --gin_file="config/models/t5_1_1/$model_size.gin" \
         --gin_file="config/runs/t5_1_1/infer.gin" \
-        --gin.INFER_OUTPUT_DIR=\"${OUTPUT_DIR}\" \
+        --gin.INFER_OUTPUT_DIR=\"${output_dir}\" \
         --alsologtostderr \
-        "$@"
+        "${other_configs[@]}"
         ;;
     *)
-        echo "$ACTION may only be one of: pretrain, finetune, eval, infer"
+        echo "$action may only be one of: pretrain, finetune, eval, infer"
         ;;
 esac
