bb21c2352cd3353142a714987bd16a66e7d21d83
theyorubayesian <akin.o.oladipo@gmail.com>
2024-06-13 00:29:30 +0000

    make scripts executable

--- scripts/tasks/aya.sh
+++ scripts/tasks/aya.sh
@@ -32,45 +32,59 @@
     mkdir -p {logs/$OUTPUT_DIR,runs/$OUTPUT_DIR}
     # ---------------------------------------------
 
-    LANGUAGES=("afrikaans")
+    # Solve the following bash task 
+    # TODO: if a variable task is set, print the task
+    # TODO: If a variable LANGUAGES exist, for each language in the list, set task to language and print task 
 
-    for language in "${LANGUAGES[@]}"
-    do
-        task="${language}_aya"
+    if [ -z "$TASKS" ]; then
+        TASKS=()
+
+        if [ -z "$LANGUAGES" ] && [ -z "$task" ]; then
+            echo "Error: All of `TASKS`, `LANGUAGES` and `task` are unset."
+            exit 1
+        fi
+    fi
+
+    if [ -n "$LANGUAGES" ]; then
+        for language in $LANGUAGES; do
+            TASKS+=("${language}_aya")
+        done
+    fi
+
+    if [ -n "$task" ]; then
+        TASKS+=("$task")
+    fi
+
+    for task in "${TASKS[@]}"; do
         train_steps=$((PRETRAINED_STEPS + FT_NUM_STEPS))
 
-        for seed in 1
-        do
-            seed_output_dir=runs/$OUTPUT_DIR/${task}_${seed}
+        bash scripts/t5_utils.sh \
+        --action finetune \
+        --task $task \
+        --feature_lengths "$FEATURE_LENGTHS" \
+        --batch_size $TRAIN_BATCH_SIZE \
+        --num_microbatches $NUM_MICROBATCHES \
+        --warmup_steps $WARMUP_STEPS \
+        --learning_rate $LEARNING_RATE \
+        --learning_rate_schedule $LEARNING_RATE_SCHEDULE \
+        --checkpoint $CHECKPOINT \
+        --checkpoint_period $CHECKPOINT_PERIOD \
+        --eval_period $EVAL_PERIOD \
+        --train_steps $train_steps \
+        --model_size $MODEL_SIZE \
+        --output_dir $seed_output_dir \
+        --gin.infer_eval/utils.DatasetConfig.batch_size=$INFER_BATCH_SIZE \
+        "${ADDITIONAL_GIN_CONFIGS[@]}" \
+        >& logs/$OUTPUT_DIR/${task}_${seed}_ft.log \
+        && finetuned=true
 
-            bash scripts/t5_utils.sh \
-            --action finetune \
-            --task $task \
-            --feature_lengths "$FEATURE_LENGTHS" \
-            --batch_size $TRAIN_BATCH_SIZE \
-            --num_microbatches $NUM_MICROBATCHES \
-            --warmup_steps $WARMUP_STEPS \
-            --learning_rate $LEARNING_RATE \
-            --learning_rate_schedule $LEARNING_RATE_SCHEDULE \
-            --checkpoint $CHECKPOINT \
-            --checkpoint_period $CHECKPOINT_PERIOD \
-            --eval_period $EVAL_PERIOD \
-            --train_steps $train_steps \
-            --model_size $MODEL_SIZE \
-            --output_dir $seed_output_dir \
-            --gin.infer_eval/utils.DatasetConfig.batch_size=$INFER_BATCH_SIZE \
-            "${ADDITIONAL_GIN_CONFIGS[@]}" \
-            >& logs/$OUTPUT_DIR/${task}_${seed}_ft.log \
-            && finetuned=true
+        checkpoints=($(ls $seed_output_dir | grep checkpoint))
 
-            checkpoints=($(ls $seed_output_dir | grep checkpoint))
-
-            if [[ $REMOVE_CHECKPOINTS == "true" ]]; then
-                for ckpt in "${checkpoints[@]}";
-                do
-                    rm -rf "$seed_output_dir/$ckpt"
-                done
-            fi
-        done
+        if [[ $REMOVE_CHECKPOINTS == "true" ]]; then
+            for ckpt in "${checkpoints[@]}";
+            do
+                rm -rf "$seed_output_dir/$ckpt"
+            done
+        fi
     done
 }
